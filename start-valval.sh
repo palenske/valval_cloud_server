e88887b41c48463e9e9b611b4e5af775723c0fdb
palenske <viniciuspalenske@gmail.com>
2024-10-14 23:59:37 -0300

    ajusta script de start

--- start-valval.sh
+++ start-valval.sh
@@ -3,38 +3,40 @@
 # Nome da instância
 INSTANCE_NAME="valheim-server"
 # AMI Ubuntu 20.04 LTS
-AMI_ID="ami-0a91cd140a1fc148a"
+AMI_ID="ami-04b107e90218672e5"
 # Tipo de instância
 INSTANCE_TYPE="t3.micro"
-# Nome do par de chaves (não será mais necessário, mas deixado aqui caso deseje usá-lo posteriormente)
-KEY_NAME="minha-chave-aws"
 # Grupo de segurança, permitindo acesso apenas pelas portas do Valheim
 SECURITY_GROUP_NAME="valheim-sg"
 REGION="us-east-1"
 ROLE_NAME="SSMRoleForEC2"
+
+# Verificar se a role já existe
+if ! aws iam get-role --role-name $ROLE_NAME --region $REGION >/dev/null 2>&1; then
+    # Criar a role IAM para permitir o uso de SSM
+    aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document file://<(echo '{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "ec2.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }')
+    # Associar políticas gerenciadas para o SSM à role criada
+    aws iam attach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore
+else
+    echo "A role $ROLE_NAME já existe. Prosseguindo..."
+fi
 
 # Criar grupo de segurança para permitir tráfego nas portas do Valheim (2456-2458)
 SG_ID=$(aws ec2 create-security-group --group-name $SECURITY_GROUP_NAME --description "SG for Valheim Server" --output text --query 'GroupId' --region $REGION)
 
 # Configurar regras do grupo de segurança (não precisa abrir a porta 22 para SSH, já que usaremos SSM)
 aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol udp --port 2456-2458 --cidr 0.0.0.0/0 --region $REGION
-
-# Criar a role IAM para permitir o uso de SSM
-aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document file://<(echo '{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "ec2.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}')
-
-# Associar políticas gerenciadas para o SSM à role criada
-aws iam attach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore
 
 # Criar uma instância EC2 com Docker e SSM ativado
 INSTANCE_ID=$(aws ec2 run-instances \
@@ -54,11 +56,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
                    -e SERVER_NAME="MeuServerValheim" \
                    -e WORLD_NAME="meu_mundo" \
                    -e SERVER_PASS="minhasenha" \
-                   lloesche/valheim-server
-                 # Instalar o SSM agent
-                 sudo snap install amazon-ssm-agent --classic
-                 sudo systemctl enable snap.amazon-ssm-agent.amazon-ssm-agent
-                 sudo systemctl start snap.amazon-ssm-agent.amazon-ssm-agent' \
+                   lloesche/valheim-server' \
     --output text --query 'Instances[0].InstanceId' --region $REGION)
 
 # Exibir status da instância
